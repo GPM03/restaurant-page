52d11d33f927b90f84223c35b0451d4be59fc4ea
Gabriel Mello <gabrielpdmello@gmail.com>
2024-12-22 15:49:35 -0300

    Rename classes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import {home} from "./home.js";
 import {menu} from "./menu.js";
 import {about} from "./about.js";
 
-const container = document.querySelector("#content");
-const homeButton = document.querySelector(".home");
-const menuButton = document.querySelector(".menu");
-const aboutButton = document.querySelector(".about");
+const container = document.querySelector(".content");
+const homeButton = document.querySelector(".home-button");
+const menuButton = document.querySelector(".menu-button");
+const aboutButton = document.querySelector(".about-button");
 
 home(container);
 
